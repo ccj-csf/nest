0d6d59bb8dcfb831b3e2ffb49da8b0b5968c304b
ccj <363992836@qq.com>
2022-07-12 15:42:12 +0800

    完成基础功能配置

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { getConfig } from './utils';
+
+@Module({
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [getConfig],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
