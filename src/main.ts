4a6e88ddf86e63143c61d24e0e5711055f1712f5
ccj <363992836@qq.com>
2022-07-18 16:27:17 +0800

    feat(*): test001

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { generateDocument } from './doc';
 declare const module: any;
+console.log('1111', 1111);
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
